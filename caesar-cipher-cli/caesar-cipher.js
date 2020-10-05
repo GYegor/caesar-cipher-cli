bbd494015b26ae3178d663e8982036318dd160d8
GYegor <yahor2011@gmail.com>
2020-10-05 11:53:43 +0300

    feat: add caesar-cipher.js with CL-arguments handling

---
+++ caesar-cipher-cli/caesar-cipher.js
@@ -0,0 +1,69 @@
+const EOL = require('os').EOL;
+const minimist = require('minimist');
+const constants = require('./constants');
+
+const intgPattern = new RegExp(/^\d+$/);
+
+const args = minimist(process.argv.slice(2), {
+  string: ['action', 'shift', 'input', 'output'],
+  alias: {
+    action: 'a',
+    shift: 's',
+    input: 'i',
+    output: 'o',
+    help: 'h',
+  },
+})
+
+const actionTypes = ['encode', 'decode'];
+
+const handleCommandLineArgs = () => {
+  if (args.help) {
+    process.exitCode = constants.exitCodes.success;
+  } else if (actionTypes.includes(args.action) && !intgPattern.test(args.shift)) {
+    console.log(actionTypes.includes(args.action), args.shift, !intgPattern.test(args.shift));
+    process.exitCode = constants.exitCodes.noShiftValue;
+  } else {
+    switch (args.action) {
+      case 'encode':
+        // TODO: handle encode
+        break;
+      case 'decode':
+        // TODO: handle decode
+        break;
+      default:
+        process.exitCode = constants.exitCodes.noActionType;
+        break;
+    }
+  }
+}
+
+handleCommandLineArgs();
+
+process.on('exit', (code) => {
+  switch (code) {
+    case constants.exitCodes.success:
+      break;
+    case constants.exitCodes.noActionType:
+      console.error(`${EOL}Valid --action not found!${EOL}`);
+      break;
+    case constants.exitCodes.noShiftValue:
+    case constants.exitCodes.noShift:
+      console.error(`${EOL}Valid --shift not found!${EOL}`);
+      break;
+    default:
+      console.error(`${EOL}Unknown error!${EOL}`);
+      break;
+  }
+  if (args.help || code !== constants.exitCodes.success) {
+    console.log(
+      `Usage: node caesar-cipher --action <action-type> --shift <value> [--input <path1>] [--output <path2>]${EOL}`,
+      `${EOL}Options:${EOL}`,
+      `  -h, --help         print node caesar-cipher command line options (currently set)${EOL}`,
+      `  -a, action         one of 'encode', 'decode'${EOL}`,
+      `  -s, shift          integer - number of chars to shift, exclude negatives, 0 and multiples of 26${EOL}`,
+      `  -i, input          absolute or relative path to text file${EOL}`,
+      `  -o, otput          absolute or relative path to text file${EOL}`,
+    )
+  }
+})
